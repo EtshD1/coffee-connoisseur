6aba8aa279cf479db2e6c0005396e96d0f1d4d7f
EtshD1 <mohamed.h.abouelenin@gmail.com>
2023-01-13 23:01:54 +0200

    Style card and add Layout component

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,13 +1,43 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const Card = ({ imgURL, alt, id, title }: { title: string; id: number; alt: string; imgURL: string }) => {
+    const [dots, setDots] = useState(false);
+    const titleRef = useRef<HTMLHeadingElement>(null);
+    const titleContainerRef = useRef<HTMLDivElement>(null);
+
+    const memoizedCallback = useCallback(() => {
+        if (titleRef.current !== null
+            && titleContainerRef.current !== null
+            && titleRef.current.offsetWidth > titleContainerRef.current.offsetWidth) {
+            setDots(ps => !ps ? true : ps);
+        } else {
+            setDots(ps => ps ? false : ps);
+        }
+    },
+        [titleRef, titleContainerRef]);
+
+    useEffect(() => {
+        memoizedCallback();
+    }, [memoizedCallback]);
+
+    useEffect(() => {
+        window.addEventListener("resize", memoizedCallback);
+        return () => window.removeEventListener("resize", memoizedCallback);
+    }, [memoizedCallback]);
+
     return (
         <Link href={`/coffee-stores/${id}`}>
-            <div>
-                <h2 className="text-4xl font-bold">{title}</h2>
+            <div className="group bg-orange-100 hover:bg-orange-200 rounded-lg p-4 flex flex-col gap-2 transition-all ease-in border-2 border-solid border-white border-opacity-30 shadow-md">
+                <div ref={titleContainerRef} className="overflow-x-hidden transition-all ease-in relative flex">
+                    <h2 ref={titleRef} className="whitespace-nowrap group-hover:opacity-100 text-xl md:text-4xl opacity-90 transition-all ease-in">{title}</h2>
+                    {dots ? <div className="pl-1 bg-orange-100 group-hover:bg-orange-200 text-xl md:text-4xl transition-all ease-in absolute right-[-1px] ">
+                        <span className="group-hover:opacity-100 opacity-90 transition-all ease-in">...</span>
+                    </div> : ""}
+                </div>
                 <div className="relative w-full h-60">
-                    <Image fill src={imgURL} alt={alt} />
+                    <Image className="object-cover rounded-md" fill src={imgURL} alt={alt} />
                 </div>
             </div>
         </Link>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,29 @@
 import Head from "next/head";
 import Banner from "../components/banner";
 import Card from "../components/card";
+import HomeLayout from "../components/layout";
+import coffeeStore from "../data/coffee-stores.json";
 
 export default function Home() {
-	return (
-		<div>
-			<Head>
-				<title>Coffee Connoisseur</title>
-				<meta
-					name="description"
-					content="Coffee Connoisseur"
-				/>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+    return (
+        <div>
+            <Head>
+                <title>Coffee Connoisseur</title>
+                <meta
+                    name="description"
+                    content="Coffee Connoisseur"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-			<main className="min-h-screen">
-				<Banner />
-                <Card title={"Bahlasawi"} id={0} alt={"Image of the Bahlasawi cafe"} imgURL={"/static/hero-image.svg"} />
-			</main>
-		</div>
-	);
+            <main className="min-h-screen">
+                <Banner />
+                <HomeLayout heading="Tronto Stores" items={coffeeStore.map(_ => ({
+                    id: _.id,
+                    name: _.name,
+                    imgHref: _.imgUrl
+                }))} />
+            </main>
+        </div>
+    );
 }
